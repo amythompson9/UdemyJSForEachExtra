c27cde3ee44f3d39be4f1880a76805206fcb1fde
Amy Thompson <amythompson9@gmail.com>
2018-06-27 20:03:49 -0400

    ForEach Examples

--- UdemyJSForEachExtra/JavaScript.js
+++ UdemyJSForEachExtra/JavaScript.js
@@ -1,1 +1,57 @@
-﻿
+﻿//myForEach(arr, func)
+
+var nums = [45, 65, 77, 34]
+
+myForEach(nums, function (num) {
+    console.log(num);
+});
+
+//function myForEach(arr, func) {
+//    func()
+//}
+
+function myForEach(arr, func) {
+    //loop through array
+    for (var i = 0; i < arr.length; i++) {
+        //call func for each item in array
+        func();
+    }
+    
+}
+
+//with a named function
+
+var colors = ["red", "orange", "yellow", "green"];
+myForEach(colors, alert)
+
+function myForEach(arr, func) {
+    //loop through array
+    for (var i = 0; i < arr.length; i++) {
+        //call func for each item in array
+        func(arr[1]);
+    }
+}
+myForEach(colors, alert)
+
+//now with an anonymous function
+myForEach(colors, function () {
+    alert("HI!")
+}
+
+myForEach(colors, function (color) {
+        console.log(color);
+    }
+
+Array.prototype.myForEach = function (func) {
+    for (var i = 0; i < this.length; i++) {
+        func(this[i]);
+    }
+        }
+
+var friends = ["charlie", "dave", "maddie", "kate"];
+
+friends.myForEach(alert)
+
+friends.myForEach(function (name) {
+    console.log("I love " + name);
+})
